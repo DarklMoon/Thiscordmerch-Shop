5ab603b9ac3aefaf0d0817300475f54391831cbd
64070237 <64070237@kmitl.ac.th>
2022-10-26 23:12:58 +0700

    Update more catagories page     - This is hard...     - Need more update     - Stress from how to link another page to a specific catagory...

--- Catagories/classic.php
+++ Catagories/classic.php
@@ -7,6 +7,7 @@
     <title>Thiscordmerch Shop</title>
     <link rel="stylesheet" href="../assets/css/NavBar.css">
     <link rel="stylesheet" href="../assets/css/Home.css">
+    <link rel="stylesheet" href="../assets/css/catagories.css">
     <!--Import Bootstrap-->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css">
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
@@ -26,7 +27,7 @@
         <li id="SideCategories">
             <nobr><a href="javascript:void(0)">Categories ▾</a></nobr>
             <ul class="sideDropdown" id="SideCategoriesDrop">
-                <li><a href="classic.html">Classic</a></li>
+                <li><a href="classic.php">Classic</a></li>
                 <li><a href="country.html">Country</a></li>
                 <li><a href="jazz.html">Jazz</a></li>
                 <li><a href="rb.html">R&B</a></li>
@@ -62,7 +63,7 @@
             <li id="ShopAll"><b><a href="#ShopAll">All</a></b></li>
             <li id="Categories"><b><a href="javascript:void(0)">Categories ▾</a></b>
                 <ul class="dropdown" id="categoriesDrop">
-                    <li><a href="classic.html">Classic</a></li>
+                    <li><a href="classic.php">Classic</a></li>
                     <li><a href="country.html">Country</a></li>
                     <li><a href="jazz.html">Jazz</a></li>
                     <li><a href="rb.html">R&B</a></li>
@@ -75,76 +76,41 @@
         </div>
         <!-- ------------------End--NavBar----------------- -->
 
-        <div class="m-5">
+        <div class="CategoriesMain">
             <h2>Catagories</h2>
             <br>
-            <table class="table">
-                <thead >
-                  <tr style="text-align: center;">
-                    <th style="border:none;"><a href="">Classic</th>
-                    <th style="border:none;">Country</th>
-                    <th style="border:none;">Jazz</th>
-                    <th style="border:none;">Pop</th>
-                    <th style="border:none;">R&B</th>
-                  </tr>
-                </thead>
-                <tbody id="detail">
-                  <tr style="height:30vh;">
-                    <td colspan="2" rowspan="2" style="border:none;">
-                        picture for song
-                    </td>
-                    <td colspan="3" rowspan="2" style="border:none;">
-                        description for song????
-                        <button class="btn btn-primary" style="bottom:10px;right:10px;">View</button>
-                    </td>
-                  </tr>
-                  <tr></tr>
-                </tbody>
-                </table>
+            <div>
+                <form class="catagoryForm" action="classic.php" method="post">
+                <div class="row mx-auto text-center">
+                    <div class="catagory col-4 col-md-2"><input type="submit" class="catagoryMenu" name="ลูกทุ่ง" value="ลูกทุ่ง"></div>
+                    <div class="catagory col-4 col-md-2"><input type="submit" class="catagoryMenu" name="T-POP" value="T-POP"></div>
+                    <div class="catagory col-4 col-md-2"><input type="submit" class="catagoryMenu" name="K-POP" value="K-POP"></div>
+                    <div class="catagory col-4 col-md-2"><input type="submit" class="catagoryMenu" name="Hiphop" value="Hip hop"></div>
+                    <div class="catagory col-4 col-md-2"><input type="submit" class="catagoryMenu" name="R&B" value="R&B"></div>
+                    <div class="catagory col-4 col-md-2"><input type="submit" class="catagoryMenu" name="Jazz" value="Jazz"></div>
+                </div>
+                </form>
+                
+                <div class="catagoryList" style="width:100%;">
+                <?php
+                    if(isset($_POST['ลูกทุ่ง'])){Show($type="ลูกทุ่ง");}
+                    else if(isset($_POST['T-POP'])){Show($type="T-POP");}
+                    else if(isset($_POST['K-POP'])){Show($type="K-POP");}
+                    else if(isset($_POST['Hiphop'])){Show($type="HIP-HOP");}
+                    else if(isset($_POST['R&B'])){Show($type="RNB");}
+                    else if(isset($_POST['Jazz'])){Show($type="Jazz");}
+
+                    function Show($type)
+                    {
+                        echo $type;
+                    }
+                ?>
+                <div>
+            </div>
         </div>
     </div>
     
     <script src="../assets/js/Home.js"></script>
     <script src="../assets/js/NavBar.js"></script>
-    <script>
-        function myFunction(x) {
-            if (x.matches) { // If media query matches
-                document.getElementById("detail").innerHTML = 
-                `<tr>
-                    <td colspan="5" style="border:none;">
-                        picture for song2
-                    </td>
-                </tr>
-                <tr>
-                    <td colspan="5" style="border:none;">
-                        description for song????2
-                        <button class="btn btn-primary" style="bottom:10px;right:10px;">View</button>
-                    </td>
-                </tr>
-            </tr>`;
-            } 
-            else {
-                document.getElementById("detail").innerHTML = 
-                `<tr style="height:30vh;">
-                    <td colspan="2" rowspan="3" style="border:none;">
-                        picture for song
-                    </td>
-                    <td colspan="3" rowspan="2" style="border:none;">
-                        description for song????
-                    </td>
-                </tr>
-                <tr></tr>
-                <tr >
-                    <td colspan="3" style="border:none;">
-                        <button class="btn btn-primary">View</button>
-                    </td>
-                </tr>`;
-            }
-        }
-
-        var x = window.matchMedia("(max-width: 600px)")
-        myFunction(x) // Call listener function at run time
-        x.addListener(myFunction)
-    </script>
 </body>
 </html>
