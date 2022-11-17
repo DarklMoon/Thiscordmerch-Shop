bf6033512a923fd3838ac59460ce6bacebe75231
64070237 <64070237@kmitl.ac.th>
2022-11-17 21:11:06 +0700

    Fix bug and change color.

--- assets/js/NavBar.js
+++ assets/js/NavBar.js
@@ -2,7 +2,7 @@ function openNav() {
   document.getElementById("SideNav").style.width = "270px";
   document.getElementById("main").style.marginLeft = "270px";
   document.getElementById("Menu").innerHTML =
-    `<a href="#Menu" onclick="closeNav()"><i class="material-icons">&#xe14c;</i></a>`;
+    `<a href="javascript:void(0)" onclick="closeNav()"><i class="material-icons">&#xe14c;</i></a>`;
   document.getElementById("main").style.position = "fixed";
 }
 
@@ -10,6 +10,6 @@ function closeNav() {
   document.getElementById("SideNav").style.width = "0";
   document.getElementById("main").style.marginLeft = "0";
   document.getElementById("Menu").innerHTML =
-    `<a href="#Menu" onclick="openNav()"><i class="material-icons">&#xe5d2;</i></a>`;
+    `<a href="javascript:void(0)" onclick="openNav()"><i class="material-icons">&#xe5d2;</i></a>`;
   document.getElementById("main").style.position = "absolute";
 }
--- assets/js/product.js
+++ assets/js/product.js
@@ -29,7 +29,7 @@ var audio = new Audio(data.sound);
 var btn_stop = document.getElementById("btn-p");
 
 function musicOn() {
-    btn_stop.innerHTML = ' <ellipse cx="65" cy="66" rx="65" ry="66" fill="#5964F2" /> <rect x="71" y="45" width="12" height="43" fill="white" /> <rect x="47" y="45" width="12" height="43" fill="white" /> '  
+    btn_stop.innerHTML = ' <ellipse cx="65" cy="66" rx="65" ry="66" fill="#116979" /> <rect x="71" y="45" width="12" height="43" fill="white" /> <rect x="47" y="45" width="12" height="43" fill="white" /> '  
     audio.play();
     audio.volume = 0.2;
     btn_stop.onclick = function() {musicOff();}
@@ -39,5 +39,5 @@ function musicOff(){
     audio.pause();
     audio.currentTime = 0;
     btn_stop.onclick = function() {musicOn();}
-    btn_stop.innerHTML = '<ellipse cx="65" cy="66" rx="65" ry="66" fill="#5964F2"/> <path d="M99 66L48.75 98.909L48.75 33.091L99 66Z" fill="white"/>';
+    btn_stop.innerHTML = '<ellipse cx="65" cy="66" rx="65" ry="66" fill="#116979"/> <path d="M99 66L48.75 98.909L48.75 33.091L99 66Z" fill="white"/>';
 }
