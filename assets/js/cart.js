9157768c71fb91113d24a7e5ccfa7e9f91726248
DarklMoon <phuriwat757@gmail.com>
2022-11-18 12:38:49 +0700

    Merge-2 BackEnd Cart NotFinish

--- assets/js/cart.js
+++ assets/js/cart.js
@@ -2,6 +2,8 @@
 // let data = JSON.parse(datas);
 
 function addProduct(){
+    document.getElementById("btn-addProduct").style.border = "none";
+    document.getElementById("btn-addProduct").style.outline = "none";
     var arrayCart = JSON.parse(localStorage.getItem("data_Cart"));
     if(arrayCart == null)
         arrayCart = [];
@@ -14,20 +16,47 @@ function addProduct(){
 
 function getProduct(){
     let orderProduct = document.getElementById("order-product");
-    document.getElementById("btn-addProduct").style.border = "none";
-    document.getElementById("btn-addProduct").style.outline = "none";
-    console.log(data);
-    console.log(orderProduct);
+    let cart_Container = document.getElementById("cart-container");
+    let arrayCart = JSON.parse(localStorage.getItem("data_Cart"));
+    let count = {};
+    if (arrayCart != null){
 
-    orderProduct.innerHTML = `<div class="product">\
-                        <img id="p1" src="${data.image}" alt="product">\
-                        <h5 class="record-name">${data.name}</h5>\
-                        <div class="product-num">\
-                            <span class="btn-minus">-</span>\
-                            <input id="num-p" type="text" value="1">\
-                            <span class="btn-plus">+</span>\
-                        </div><br>\
-                        <h5>$10.00</h5>\
-                        </div>`
+        arrayCart.forEach(element => {
+            // console.log(JSON.stringify(element));
+            count[JSON.stringify(element)] = (count[JSON.stringify(element)] || 0) +1;
+        });
+        console.log(count);
+        console.log(Object.entries(count));
+
+        Object.entries(count).forEach((element) => {
+            let eachProduct = JSON.parse(element[0]);
+            console.log(eachProduct.image);
+            console.log(element[1]);
+            console.log(element[1] * 10);
+        });
+        
+        orderProduct.innerHTML = `<div class="product">\
+                            <img id="p1" src="${arrayCart[0].image}" alt="product">\
+                            <h5 class="record-name">${arrayCart[0].name}</h5>\
+                            <div class="product-num">\
+                                <span class="btn-minus">-</span>\
+                                <input id="num-p" type="text" value="1">\
+                                <span class="btn-plus">+</span>\
+                            </div><br>\
+                            <h5>$10.00</h5>\
+                            </div>`;
+    }
+    else{
+        cart_Container.innerHTML = `<div class="header">\
+                                        <h2><b>Your cart</b></h2>\
+                                        <hr class="hr-line1">\
+                                    </div><br>
+                                    <div style="margin-bottom:20vh;">
+                                        <center>
+                                            <p>Your cart is currently empty.</p><br>
+                                            <p>Continue browsing here.</p>
+                                        </center>
+                                    </div>`;
+    }
 
 }
