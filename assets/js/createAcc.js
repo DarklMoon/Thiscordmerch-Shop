da0bf7a069e8ffd7dcdcc97dc4e497a81a83887b
AomRerk <64070094@it.kmitl.ac.th>
2022-11-19 14:34:28 +0700

    Finish create&login

--- assets/js/createAcc.js
+++ assets/js/createAcc.js
@@ -1,27 +1,43 @@
 function Create() {
     // alert("hi")
-    var inpFname= document.getElementById("fname");
-    var inpLname= document.getElementById("lname");
-    var inpEmail= document.getElementById("email");
-    var inpPass= document.getElementById("password1");
-
+    var inpFname = document.getElementById("fname");
+    var inpLname = document.getElementById("lname");
+    var inpEmail = document.getElementById("email");
+    var inpPass1 = document.getElementById("password1");
+    var inpPass2 = document.getElementById("password2");
+    var check = false
     var toarr
+    var userData = {
+        "fullname": inpFname.value,
+        "lastname": inpLname.value,
+        "email": inpEmail.value,
+        "pass": inpPass1.value,
+    }
 
     if (localStorage.getItem("userData") == null) {
         toarr = []
-    } else{
+    } else {
         toarr = JSON.parse(localStorage.getItem("userData"))
-    }
-    // console.log(toarr)
-    var userData = {
-        "fullname" : inpFname.value ,
-        "lastname" : inpLname.value ,
-        "email" : inpEmail.value ,
-        "pass" : inpPass.value ,
-    }
-    // console.log(userData)
 
-    toarr.push(userData)
-    localStorage.setItem("userData", JSON.stringify(toarr))
-    // console.log(toarr)
+    }
+
+    if (inpFname.value.length < 5 || inpLname.value.length < 5) {
+        alert("Name is too short")
+        check = true
+    }
+    if (inpEmail.value.length < 5) {
+        alert("Email is invalid")
+        check = true
+    } 
+    if (inpPass1.value !== inpPass2.value) {
+        alert("Password do not match")
+        check = true
+    } 
+    if(!check){
+        toarr.push(userData)
+        localStorage.setItem("userData", JSON.stringify(toarr))
+        alert("Already created your Account")
+        location.assign("login.html")
+    }
+
 }
