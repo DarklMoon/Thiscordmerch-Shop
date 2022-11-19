c06d812e33429436a722031b168bc6ed4ceb68f2
DarklMoon <phuriwat757@gmail.com>
2022-11-19 18:29:37 +0700

    Merge-5 WaitToFixCart

--- assets/js/cart.js
+++ assets/js/cart.js
@@ -1,68 +1,44 @@
-// let datas = localStorage.getItem("data_Product");
-// let data = JSON.parse(datas);
-
 function addProduct(){
     document.getElementById("btn-addProduct").style.border = "none";
     document.getElementById("btn-addProduct").style.outline = "none";
     let dataInArray = JSON.parse(`{"${data.name}" : [1,${datas}]}`);
     var arrayCart = JSON.parse(localStorage.getItem("data_Cart"));
+    let check = false;
     if(arrayCart == null)
         arrayCart = [];
-    console.log(dataInArray);
-    // console.log(Object.keys(arrayCart).length == 0);
         
     if (Object.keys(arrayCart).length != 0) {
-      console.log(1);
-      arrayCart.forEach((element) => {
-        let eachKeyArray = Object.keys(element)[0];
-        let eachNameProduct = JSON.parse(localStorage.getItem("data_Product")).name;
-        if(eachKeyArray == eachNameProduct) {
-            console.log(arrayCart);
-            Object.values(element)[0][0] += 1;
-        } 
-        else {
-          arrayCart.push(dataInArray);
-        }
-      });
-    } else {
-        console.log(2);
+        arrayCart.forEach((element) => {
+            let eachKeyArray = Object.keys(element)[0];
+            let eachNameProduct = JSON.parse(localStorage.getItem("data_Product")).name;
+            if(eachKeyArray == eachNameProduct) {
+                Object.values(element)[0][0] += 1;
+                check = true;
+            } 
+        });
+    } else{
+        check = true;
         arrayCart.push(dataInArray);
     }
-    console.log(arrayCart);
-    // console.log(arrayCart);
+    if(check == false){
+        arrayCart.push(dataInArray);
+    }
+
     localStorage.setItem("data_Cart", JSON.stringify(arrayCart));
     
 }
 
+let num_Product = 0;
 
 function getProduct(){
     let orderProduct = document.getElementById("order-product");
     let cart_Container = document.getElementById("cart-container");
     let arrayCart = JSON.parse(localStorage.getItem("data_Cart"));
     let totalCost = document.getElementById("cart-total");
-    let count = {};
-    let num = 0;
     let allCost = 0;
-    if (arrayCart != null){
-
-        arrayCart.forEach(element => {
-            console.log((Object.keys(element))[0]);
-            console.log(JSON.parse(localStorage.getItem("data_Product")).name);
-            if (Object.keys(element)[0] == JSON.parse(localStorage.getItem("data_Product")).name){
-                // Object.values(element)[0][0] += 1;
-                console.log((Object.values(element))[0]);
-            }
-              count[JSON.stringify(element)] =
-                (count[JSON.stringify(element)] || 0) + 1;
-        });
-        sessionStorage.setItem("amount_Product", JSON.stringify(count));
-        // console.log(Object.entries(count));
-
+    if ((arrayCart != null) && (Object.keys(arrayCart).length != 0)){
         Object.values(arrayCart).forEach((element) => {
             let eachProduct = (Object.values(element))[0];
-            // console.log(eachProduct);
-            // console.log(element[1]);
-            // console.log(element[1] * 10);
 
             orderProduct.innerHTML += `<div class="product">\
                                 <img id="p1" src="${
@@ -72,15 +48,15 @@ function getProduct(){
                                   eachProduct[1].name
                                 }</h5>\
                                 <div class="product-num">\
-                                    <span id="btn-minus" onclick="buttonMinus(${num})" style="cursor: default;">-</span>\
-                                    <input class="num-p" id="num-p-${num}" type="text" value="${eachProduct[0]}">\
-                                    <span id="btn-plus" onclick="buttonPlus(${num})" style="cursor: default;">+</span>\
+                                    <span id="btn-minus" onclick="buttonMinus(${num_Product})" style="cursor: default;">-</span>\
+                                    <input class="num-p" id="num-p-${num_Product}" type="text" value="${eachProduct[0]}">\
+                                    <span id="btn-plus" onclick="buttonPlus(${num_Product})" style="cursor: default;">+</span>\
                                 </div><br>\
                                 <h5>$${eachProduct[0] * 10}.00</h5>\
                                 <hr class="hr-line1">\
                                 </div>`;
-            allCost += element[1]*10;
-            num += 1;                                                           
+            allCost += eachProduct[0]*10;
+            num_Product += 1;                                                           
         });
     }
     else{
@@ -91,7 +67,7 @@ function getProduct(){
                                     <div style="margin-bottom:20vh;">
                                         <center>
                                             <p>Your cart is currently empty.</p><br>
-                                            <p>Continue browsing here.</p>
+                                            <a href="index.html"><p>Continue browsing here.</p></a>
                                         </center>
                                     </div>`;
     }
@@ -112,21 +88,35 @@ function buttonPlus(num){
 }
 
 function updateCart(){
-    // let countProduct = JSON.parse(sessionStorage.getItem("amount_Product"));
-    // let arrayCart = JSON.parse(localStorage.getItem("data_Cart"));
-
-    // console.log(count);
-    // countProduct.forEach(element => console.log(element));
-
-
-    // arrayCart.forEach((element) => {
-    //   console.log(element);     
-    // });
-
-
-    // location.reload();
+    let arrayCart = JSON.parse(localStorage.getItem("data_Cart"));
+    console.log(arrayCart);
+    arrayCart.forEach(loop);
+    function loop(item, index){
+        let num_EachProduct = document.getElementById(`num-p-${index}`).value;
+        if(num_EachProduct > 0){
+            Object.values(item)[0][0] = num_EachProduct;
+        }
+        else{
+            arrayCart.splice(index, 1);
+        }
+    }
+    localStorage.setItem("data_Cart", JSON.stringify(arrayCart));
+    location.reload();
 }
 
 function buyProduct(){
+    let cart_Container = document.getElementById("cart-container");
+    
+    cart_Container.innerHTML = `<div class="header">\
+                                        <h2><b>Thanks for supporting us!</b></h2>\
+                                        <hr class="hr-line1">\
+                                    </div><br>
+                                    <div style="margin-bottom:20vh;">
+                                        <center>
+                                            <p>You have successfully purchased from our website.</p><br>
+                                            <a href="index.html"><p>Do you want to buy more products?</p></a>
+                                        </center>
+                                    </div>`;
+    localStorage.removeItem("data_Cart");
 
 }
