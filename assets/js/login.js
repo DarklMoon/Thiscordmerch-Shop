079dc50b9b160871eeb89d91c2feb1d4418e23e3
DarklMoon <phuriwat757@gmail.com>
2022-11-20 11:06:24 +0700

    AddTextWhenLogin

--- assets/js/login.js
+++ assets/js/login.js
@@ -29,10 +29,19 @@ var logIn = document.getElementsByClassName("login")[0]
 if (localStorage.getItem("Account")) {
     logIn.innerHTML = ""
     var newElm = document.createElement("h1")
+    var hr = document.createElement("hr")
+    var centerTage = document.createElement("center")
+    var p_tag = document.createElement("p")
+    var p_text = document.createTextNode("You have successfully logged in.")
     var txtName = document.createTextNode(localStorage.getItem("Account"))
     newElm.appendChild(txtName)
-    newElm.setAttribute("class", "mb-4")
+    newElm.setAttribute("class", "mb-2")
+    centerTage.appendChild(hr);
+    centerTage.setAttribute("class", "mb-4")
+    p_tag.appendChild(p_text)
     logIn.appendChild(newElm)
+    logIn.appendChild(centerTage)
+    logIn.appendChild(p_tag)
     var newBtn = document.createElement("button")
     var txtBtn = document.createTextNode("Log out")
     newBtn.appendChild(txtBtn)
